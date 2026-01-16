74f09eac92050d6291b93c858c9a73c3830903b1
secret <zadoronidanil@gmail.com>
2026-01-17 04:17:57 +0700

    Update testest.lua

--- testest.lua
+++ testest.lua
@@ -1,4 +1,3 @@
-print("а")
 local Lighting = game:GetService("Lighting")
 local RunService = game:GetService("RunService")
 local LocalPlayer = game:GetService("Players").LocalPlayer
@@ -3948,7 +3947,9 @@ Components.TitleBar = (function()
         local CheckGradient = New("UIGradient", {
             Color = ColorSequence.new({
                 ColorSequenceKeypoint.new(0, Color3.fromRGB(100, 100, 100)),
+                ColorSequenceKeypoint.new(0.2, Color3.fromRGB(100, 100, 100)),
                 ColorSequenceKeypoint.new(0.5, Color3.fromRGB(255, 255, 255)),
+                ColorSequenceKeypoint.new(0.8, Color3.fromRGB(100, 100, 100)),
                 ColorSequenceKeypoint.new(1, Color3.fromRGB(100, 100, 100)),
             }),
             Parent = SubtitleLabel,
@@ -3957,10 +3958,10 @@ Components.TitleBar = (function()
         task.spawn(function()
             local success, TweenService = pcall(game.GetService, game, "TweenService")
             if success and TweenService then
-
+                -- Move from -0.8 to 0.8 to minimize "dead time" (solid grey state)
                 local tInfo = TweenInfo.new(3, Enum.EasingStyle.Linear, Enum.EasingDirection.InOut, -1, false)
-                CheckGradient.Offset = Vector2.new(-1, 0)
-                local tween = TweenService:Create(CheckGradient, tInfo, {Offset = Vector2.new(1, 0)})
+                CheckGradient.Offset = Vector2.new(-0.8, 0)
+                local tween = TweenService:Create(CheckGradient, tInfo, {Offset = Vector2.new(0.8, 0)})
                 tween:Play()
             end
         end)
